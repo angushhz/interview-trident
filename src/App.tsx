37d057fb0aa7a961f7b38d8f0823bc26d0335d78
angushhz <hoangnamqt.2001@gmail.com>
2026-01-30 13:37:32 +0700

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
-function ProgressBar() {
-  return (
-    <div className="progress-bar-container">
-      <div className="progress-bar-fill" />
-    </div>
-  )
-}
 
 function App() {
-  const [bars, setBars] = useState<number[]>([])
-
-  const addBar = () => {
-    setBars((prev) => [...prev, Date.now()])
-  }
 
   return (
     <div className="app">
-      <button onClick={addBar}>Add</button>
-      <div className="progress-bars">
-        {bars.map((id) => (
-          <ProgressBar key={id} />
-        ))}
-      </div>
+      <h1>Hello World</h1>
     </div>
   )
 }
